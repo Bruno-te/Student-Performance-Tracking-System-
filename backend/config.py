e25d6bdb95a5e323eb1feb8a909caf76ac1a55e4
c-hirwa <c.hirwa@alustudent.com>
2025-07-07 07:39:43 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://your_user:your_pass@localhost/urugendo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Jamorant12@localhost/urugendo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
