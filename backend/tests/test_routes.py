ec0c629cd64be8ea0444b978697832e6e1719b9f
c-hirwa <c.hirwa@alustudent.com>
2025-07-07 07:07:54 +0200

    Create test_routes.py

---
+++ backend/tests/test_routes.py
@@ -0,0 +1,15 @@
+import json
+from backend.app import app
+from models import db
+
+def test_add_student():
+    client = app.test_client()
+    response = client.post('/students/', json={
+        "full_name": "Test Student",
+        "gender": "Male",
+        "date_of_birth": "2004-05-10",
+        "class_id": 1,
+        "guardian_contact": "0788123456"
+    })
+    assert response.status_code == 201
+    assert b"Student added" in response.data
