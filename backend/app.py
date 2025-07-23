260b759948309a4767419ab62b8c2c6dc6599bd6
Celine SHOGA <c.shoga@alustudent.com>
2025-07-23 11:02:14 +0200

    delete

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ app.register_blueprint(attendance_bp, url_prefix='/attendance')
 app.register_blueprint(teacher_assignments_bp, url_prefix='/api/teacher-assignments')
 app.register_blueprint(assessments_bp, url_prefix='/api/assessments')
 app.register_blueprint(behavior_bp, url_prefix='/api/behavior' ) 
-app.register_blueprint(behavioral_bp) url_prefix='/api/behavioral') 
+app.register_blueprint(behavioral_bp, url_prefix='/api/behavioral')
 app.register_blueprint(participation_bp, url_prefix='/api/participation')
 # main
 
